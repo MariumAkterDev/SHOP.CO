e0a1e5ed590c1615c5ae7b5e3df81f2153962489
MariumAkterDev <mariumshorna8@gmail.com>
2025-01-15 01:20:04 +0600

    updates commit

--- src/Components/Breadcrumb/Breadcrumb.jsx
+++ src/Components/Breadcrumb/Breadcrumb.jsx
@@ -18,9 +18,9 @@ const Breadcrumb = () => {
             <div className="container">
                 <div className="bread_Row">
                     <ul>
-                        <li><Link to='/' className='text=[12px] md:text-[16px] text-lightColor-2C font-poppins font-normal' >Home</Link></li>
-                        <li className='text-[#000] text-[14px]  md:text-[16px] mt-[2px]'><IoChevronForward/></li>
-                        <li><Link to='/shop'className='text-[12px] md:text-[18px] text-[#000] font-poppins font-normal' >{path}</Link></li>
+                        <li><Link to='/' className='text=[10px] md:text-[16px] text-lightColor-2C font-poppins font-normal' >Home</Link></li>
+                        <li className='text-[#000] text-[12px]  md:text-[16px] mt-[2px]'><IoChevronForward/></li>
+                        <li><Link to='/shop'className='text-[10px] md:text-[18px] text-[#000] font-poppins font-normal' >{path}</Link></li>
                     </ul>
                 </div>
             </div>
